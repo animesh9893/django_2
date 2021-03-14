2bfb618558d558481c5350b3f608cffda34b157f
Animesh Shrivatri <animesh.shrivatri2002@gmail.com>
2021-03-14 10:21:21 +0530

    add login and sign up models and html's

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 
 urlpatterns = [
-
+    path('', views.home,name="home"),
+    path('log_in/', views.log_in,name="log_in"),
+    path('log_out/', views.log_out,name="log_out"),
+    path('sign_up/', views.sign_up,name="sign_up"),
 ]
